75d4e9d3cd54a9450cac8093424c0f816c15df33
ryankosh <support@ryadcorp.com>
2015-02-19 11:50:56 -0600

    Create woocommerce-wholesale-quick-edit-admin.js

---
+++ admin/js/woocommerce-wholesale-quick-edit-admin.js
@@ -0,0 +1,60 @@
+// @ref http://rachelcarden.com/2012/03/manage-wordpress-posts-using-bulk-edit-and-quick-edit/
+(function($) {
+    // we create a copy of the WP inline edit post function
+    var $wp_inline_edit = inlineEditPost.edit;
+    // and then we overwrite the function with our own code
+    inlineEditPost.edit = function( id ) {
+        // "call" the original WP edit function
+        // we don't want to leave WordPress hanging
+        $wp_inline_edit.apply( this, arguments );
+
+        // now we take care of our business
+
+        // get the post ID
+        var $post_id = 0;
+        if ( typeof( id ) == 'object' )
+            $post_id = parseInt( this.getId( id ) );
+
+        if ( $post_id > 0 ) {
+            // define the edit row
+            var $edit_row = $( '#edit-' + $post_id );
+            var $post_row = $( '#post-' + $post_id );
+			
+            // get the data
+            var $wholesale_price = $( '.column-wholesale', $post_row ).html();
+			// strip the $ and/or --
+			var $wholesale_price = $wholesale_price.replace("$", "");
+			var $wholesale_price = $wholesale_price.replace("--", "");
+			
+            // populate the data
+            $( ':input[name="wholesale_price"]', $edit_row ).val( $wholesale_price );		
+        }
+    };
+
+    $( '#bulk_edit' ).live( 'click', function() {
+        // define the bulk edit row
+        var $bulk_row = $( '#bulk-edit' );
+
+        // get the selected post ids that are being edited
+        var $post_ids = new Array();
+        $bulk_row.find( '#bulk-titles' ).children().each( function() {
+            $post_ids.push( $( this ).attr( 'id' ).replace( /^(ttle)/i, '' ) );
+        });
+
+        // get the data
+        var $wholesale_price = $bulk_row.find( 'input[name="wholesale_price"]' ).val();
+
+        // save the data
+        $.ajax({
+            url: ajaxurl, // this is a variable that WordPress has already defined for us
+            type: 'POST',
+            async: false,
+            cache: false,
+            data: {
+                action: 'save_bulk_edit_wholesale', // this is the name of our WP AJAX function that we'll set up next
+                post_ids: $post_ids, // and these are the 2 parameters we're passing to our function
+                wholesale_price: $wholesale_price
+            }
+        });
+    });
+})(jQuery);
